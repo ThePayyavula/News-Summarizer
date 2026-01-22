beb8c46d94de64daf7911bc512f8b3caaee5149f
ThePayyavula <rohan.payyavula.cs@gmail.com>
2026-01-22 17:44:49 -0500

    module.exports of newsService, aiService, and cacheService

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const newsService = require('./newsService');
+const aiService = require('./aiService');
+const cacheService = require('./cacheService');
+
+module.exports = {
+  newsService,
+  aiService,
+  cacheService,
+};
