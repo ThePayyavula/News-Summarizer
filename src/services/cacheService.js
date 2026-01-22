ce4840f8f35275a40cc0ea6aca4cd74f3ab77b4b
ThePayyavula <rohan.payyavula.cs@gmail.com>
2026-01-22 16:42:49 -0500

    CacheService Setup of Functions

---
+++ src/services/cacheService.js
@@ -0,0 +1,44 @@
+/**
+ * Simple in-memory cache service
+ */
+
+const { CACHE_CONFIG } = require('../config');
+
+class CacheService {
+    constructor() {}
+    /**
+   * Check if cache is still valid
+   * @returns {boolean}
+   */
+  isValid() {
+
+  }
+  /**
+   * Get cached articles
+   * @returns {Array|null}
+   */
+  getArticles() {}
+
+  /**
+   * Set articles in cache
+   * @param {Array} articles
+   */
+  setArticles(articles) {
+    
+  }
+  /**
+   * Get cache age in milliseconds
+   * @returns {number|null}
+   */
+  getAge() {
+    
+  }
+  /**
+   * Clear the cache
+   */
+  clear() {
+    
+  }
+
+
+}
